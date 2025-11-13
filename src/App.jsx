902dc602fc15c438ec71427d89905c9f00a6554a
dipujafar <dipujafarmr@gmail.com>
2025-11-13 13:11:42 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import "./App.css";
 import Users from "./components/Users";
 import Loading from "./components/Loading";
@@ -10,21 +10,24 @@ import { fetchUserData } from "./api";
 
 function App() {
   const [value, setValue] = useState(0);
-  const []
-  // const  usersData  = fetchUserData();
+  // const [usersData, setUsersData] = useState([]);
+  const  usersData  = fetchUserData();
 
   // const handleIncreaseValue = () => {
   //   setValue(value + 1);
   // };
 
-
-  
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => setUsersData(data));
+  // }, [usersData]);
 
   // API
   return (
     <>
       {/* <h1 className="text-3xl">{value}</h1> */}
-      <Suspense fallback={<Loading/>}>
+      <Suspense fallback={<Loading />}>
         <Users usersData={usersData}></Users>
       </Suspense>
 
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -8,7 +8,7 @@ export default function Users({ usersData }) {
       <h1 className="text-2xl text-center mb-5">Total Users {users.length}</h1>
       <div className="grid lg:grid-cols-5 md:grid-cols-3  grid-cols-1 gap-5">
         {
-           users.map(user=> <UserCard key={user.id} user={user}></UserCard>)
+           users.map(user=> <UserCard key={user?.id} user={user}></UserCard>)
         }
       </div>
     </div>
